8f657fab05ee4fdb25c059d32e1779d8559b3ccc
Matheus Amorim <mtsgamorim@gmail.com>
2023-01-21 15:40:02 -0300

    feat: create route user post

--- src/main/java/com/tweteroo/api/controller/UserController.java
+++ src/main/java/com/tweteroo/api/controller/UserController.java
@@ -1,11 +1,13 @@
 package com.tweteroo.api.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tweteroo.api.dto.UserDTO;
+import com.tweteroo.api.service.UserService;
 
 import jakarta.validation.Valid;
 
@@ -13,8 +15,11 @@ import jakarta.validation.Valid;
 @RequestMapping("/sign-up")
 public class UserController {
     
+    @Autowired
+    private UserService service;
+
     @PostMapping
     public void create(@RequestBody @Valid UserDTO req) {
-        System.out.println(req);
+        service.create(req);
     }
 }
---
+++ src/main/java/com/tweteroo/api/service/UserService.java
@@ -0,0 +1,19 @@
+package com.tweteroo.api.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.tweteroo.api.dto.UserDTO;
+import com.tweteroo.api.model.User;
+import com.tweteroo.api.repository.UserRepository;
+
+@Service
+public class UserService {
+    
+    @Autowired
+    private UserRepository repository;
+
+    public void create(UserDTO dto) {
+        repository.save(new User(dto));
+    }
+}
