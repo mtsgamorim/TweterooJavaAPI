671cce11859e0ecb98d253a2d2790757b3243c76
Matheus Amorim <mtsgamorim@gmail.com>
2023-01-21 14:59:14 -0300

    feat: create TweetDTO and UserDTO

---
+++ src/main/java/com/tweteroo/api/dto/UserDTO.java
@@ -0,0 +1,16 @@
+package com.tweteroo.api.dto;
+
+import jakarta.validation.constraints.NotBlank;
+
+
+public record UserDTO(
+    
+    @NotBlank
+    String username, 
+    
+    @NotBlank
+    String avatar) {
+}
+
+
+
