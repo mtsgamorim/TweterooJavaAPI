5ca5adb17a0ce723c54c2c0716aa833858dea584
Matheus Amorim <mtsgamorim@gmail.com>
2023-01-21 15:39:30 -0300

    feat: create constructors in models

--- src/main/java/com/tweteroo/api/model/Tweet.java
+++ src/main/java/com/tweteroo/api/model/Tweet.java
@@ -1,15 +1,27 @@
 package com.tweteroo.api.model;
 
+import com.tweteroo.api.dto.TweetDTO;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Entity
+@NoArgsConstructor
 public class Tweet {
+
+    public Tweet(TweetDTO data) {
+        this.avatar = data.avatar();
+        this.username = data.username();
+        this.text = data.text();
+    }
+
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
--- src/main/java/com/tweteroo/api/model/User.java
+++ src/main/java/com/tweteroo/api/model/User.java
@@ -1,15 +1,24 @@
 package com.tweteroo.api.model;
 
+import com.tweteroo.api.dto.UserDTO;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Entity
+@NoArgsConstructor
 public class User {
+
+    public User(UserDTO data) {
+        this.avatar = data.avatar();
+        this.username = data.username();
+    }
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
